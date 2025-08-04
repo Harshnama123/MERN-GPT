423d96ef9abd470566af7d8d2b2c57efe30a49a1
Harshnama123 <harshnama1234@gmail.com>
2025-08-05 00:52:49 +0530

    backend fix

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,7 +8,14 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://mern-gpt-1-git-main-harshnama123s-projects.vercel.app",
+    "https://mern-gpt.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
